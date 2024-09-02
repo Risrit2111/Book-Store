b42a4514359a08b29b2c079ac34b01361b167856
Kosaraju Jyothsna Abhay <kjabhay@gmail.com>
2024-09-02 21:55:31 +0530

    Final Commit

--- backend/index.js
+++ backend/index.js
@@ -1,129 +1,41 @@
-const express = require("express");
-const mongoose = require("mongoose");
-require('dotenv').config();
-const Book = require("./models/bookModel.js");
-const { CURSOR_FLAGS } = require("mongodb");
+import express from 'express';
+import { PORT, mongoDBURL } from './config.js';
+import mongoose from 'mongoose';
+import booksRoute from './routes/booksRoute.js';
+import cors from 'cors';
+
 const app = express();
 
-app.use(express.urlencoded({extended: true}));
+// Middleware for parsing request body
 app.use(express.json());
-app.get("/", (req, res) => {
-    console.log(req);
-    return res.status(234).send('Welcome to the Store');
+
+// Middleware for handling CORS POLICY
+// Option 1: Allow All Origins with Default of cors(*)
+app.use(cors());
+// Option 2: Allow Custom Origins
+// app.use(
+//   cors({
+//     origin: 'http://localhost:3000',
+//     methods: ['GET', 'POST', 'PUT', 'DELETE'],
+//     allowedHeaders: ['Content-Type'],
+//   })
+// );
+
+app.get('/', (request, response) => {
+  console.log(request);
+  return response.status(234).send('Welcome To MERN Stack Tutorial');
 });
 
-const createNewBook = async (newBook) => {
-    try {
-        const book = await Book.create(newBook);
-        console.log('Book created:', book);
-    } catch (error) {
-        console.error('Error creating book:', error);
-    }
-};
-
-app.post("/books", (req, res) => {
-    try{
-        if(
-            !req.body.title ||
-            !req.body.author ||
-            !req.body.publishYear
-        ){
-            return res.status(400).send({
-                message: "Send all required fields: title, author, publishYear"
-            });
-        }
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear
-        };
-        createNewBook(newBook);
-    }
-    catch(error){
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-    }
-})
-
-app.get('/books', async (req, res) => {
-    try{
-        const books = await Book.find({});
-
-        return res.status(200).json({
-            count: books.length,
-            data: books
-        });
-    }
-    catch(error){
-        console.log(error.message);
-        res.status(500).send({ message: error.message});
-    }
-});
-
-app.get('/books/:id', async (req, res) => {
-    try{
-
-        const {id} = req.params
-        const book = await Book.findById(id);
-
-        return res.status(200).json(book);
-    }
-    catch(error){
-        console.log(error.message);
-        res.status(500).send({ message: error.message});
-    }
-});
-
-app.put("/books/:id", async (req,res) => {
-    try{
-        if(
-            !req.body.title ||
-            !req.body.author ||
-            !req.body.publishYear
-        ){
-            return res.status(400).send({
-                message: "Send all required fields: title, author, publishYear"
-            });
-        }
-        const {id} = req.params;
-        const result = await Book.findByIdAndUpdate(id, req.body);
-
-        if(!result){
-            return res.status(404).json({message: "Book not Found"});
-        }
-        return res.status(200).send({message: "Book Updated Successfully"});
-    }
-    catch(error){
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-    }
-});
-
-app.delete("/books/:id" , async (req, res) => {
-    try{
-        const {id} = req.params;
-
-        const result = await Book.findByIdAndDelete(id);
-
-        if(!result){
-            return res.status(404).json({message: "Book not found"});
-        }
-        return res.status(200).send({message: "Book Deleted Successfully"});
-    } 
-    catch(error){
-        console.log(error.message);
-        res.status(500).send({message: error.message});
-    }
-});
+app.use('/books', booksRoute);
 
 mongoose
-    .connect(process.env.MONGO_URI)
-    .then(() => {
-        console.log("App connected Successfully");
-        app.listen(process.env.PORT || 5555, () => {
-            console.log(`Your app is listening on port ${process.env.PORT}`);
-        })
-    })
-    .catch((error) => {
-        console.log(error);
+  .connect(mongoDBURL)
+  .then(() => {
+    console.log('App connected to database');
+    app.listen(PORT, () => {
+      console.log(`App is listening to port: ${PORT}`);
     });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
--- backend/models/bookModel.js
+++ backend/models/bookModel.js
@@ -1,14 +1,23 @@
-const mongoose = require("mongoose")
+import mongoose from 'mongoose';
 
-const bookSchema = mongoose.Schema({
-    title: {type: String, required: true},
-    author: {type: String, required: true},
-    publishYear: {type: Number, required: true}
-},
-{
-    timestamps: true
-})
+const bookSchema = mongoose.Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    author: {
+      type: String,
+      required: true,
+    },
+    publishYear: {
+      type: Number,
+      required: true,
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
 
-const Book = mongoose.model('Book', bookSchema);
-
-module.exports = Book;
+export const Book = mongoose.model('Book', bookSchema);
